192840a4c26588285035b2e9da7e2beaa79c4944
Nittaany <nitantchaudhary123@gmail.com>
2023-12-10 09:59:12 +0530

    added event listner

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+document.querySelector(".check").addEventListener("click",function(){
+    const guessed_no= document.querySelector(".guess").value;
+    console.log(guessed_no);
+})
